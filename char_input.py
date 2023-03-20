27df95c12870f04d8b1607290162e15df6589975
nikashdeka <nikash.deka@gmail.com>
2023-03-20 15:52:07 +0800

    Commit: Python exercise - character input

---
+++ char_input.py
@@ -0,0 +1,6 @@
+
+
+def greeting_user():
+    print("Hello!")
+    name =input("Tell me your name: \n")
+    print(f"Hello {name}, Welcome to Python Exercises!")
---
+++ main.py
@@ -0,0 +1,3 @@
+import char_input
+
+char_input.greeting_user()
